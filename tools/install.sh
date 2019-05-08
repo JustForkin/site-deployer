30f79146b4efecebe9f1df516453fff506143518
Martin Bouillaud <bouillaud.martin@gmail.com>
2019-05-08 21:25:17 +0200

    Fix alias creation

--- tools/install.sh
+++ tools/install.sh
@@ -6,12 +6,12 @@ then
     git clone git@github.com:bilyboy785/site-deployer.git /opt/site-deployer >/dev/null 2>&1
     if [ -f ~/.zshrc ]
     then
-        echo 'alias deploy="bash /opt/site-deployer/deploy.sh"' >> "~/.zshrc"
-        source ~/.zshrc
+        echo 'alias deploy="bash /opt/site-deployer/deploy.sh"' >> /root/.zshrc
+        source /root/.zshrc
     elif [ -f ~/.bashrc ]
     then
-        echo 'alias deploy="bash /opt/site-deployer/deploy.sh"' >> "~/.bashrc"
-        source ~/.bashrc
+        echo 'alias deploy="bash /opt/site-deployer/deploy.sh"' >> /root/.bashrc
+        source /root/.bashrc
     fi
 else
     echo "-> Please execute as root !"
