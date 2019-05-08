0aa8b4f7d8ad2fa2415f131847a6a5f1e7a0df22
Martin Bouillaud <bouillaud.martin@gmail.com>
2019-05-08 21:24:07 +0200

    Fix install script

--- tools/install.sh
+++ tools/install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-case $(whoami) in
-    root)
-        echo "# Starting install of Site Deployer"
-        git clone git@github.com:bilyboy785/site-deployer.git /opt/site-deployer
-        if [ -f ~/.zshrc ]
-        then
-            BASHRC_FILE="~/.zshrc"
-        elif [ -f ~/.bashrc ]
-        then
-            BASHRC_FILE="~/.bashrc"
-        fi
-
-        echo 'alias deploy="bash /opt/site-deployer/deploy.sh"' >> $BASHRC_FILE
-        source $BASHRC_FILE
-        ;;
-    *)
-        echo "-> Please execute as root !"
-        ;;
-esac
+if [ "$(whoami)" == "root" ]
+then
+    echo "# Starting install of Site Deployer"
+    git clone git@github.com:bilyboy785/site-deployer.git /opt/site-deployer >/dev/null 2>&1
+    if [ -f ~/.zshrc ]
+    then
+        echo 'alias deploy="bash /opt/site-deployer/deploy.sh"' >> "~/.zshrc"
+        source ~/.zshrc
+    elif [ -f ~/.bashrc ]
+    then
+        echo 'alias deploy="bash /opt/site-deployer/deploy.sh"' >> "~/.bashrc"
+        source ~/.bashrc
+    fi
+else
+    echo "-> Please execute as root !"
+fi
