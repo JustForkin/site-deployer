fa18955a12036da62e867ad558bd9a4afdf43a1a
Martin Bouillaud <martin.bouillaud@fr.clara.net>
2019-05-09 13:17:18 +0200

    Fix install script

--- tools/install.sh
+++ tools/install.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-CURRENT_USER=$(whoami)
-
-if [ "$CURRENT_USER" = "root" ]
+echo "# Starting install of Site Deployer"
+git clone git@github.com:bilyboy785/site-deployer.git ~/site-deployer >/dev/null 2>&1
+if [ -f ~/.zshrc ]
 then
-    echo "# Starting install of Site Deployer"
-    git clone git@github.com:bilyboy785/site-deployer.git $(dirname "$0") >/dev/null 2>&1
-    if [ -f ~/.zshrc ]
-    then
-        echo 'alias deploy="bash $(dirname "$0")/deploy.sh"' >> /root/.zshrc
-        source /root/.zshrc
-    elif [ -f ~/.bashrc ]
-    then
-        echo 'alias deploy="bash $(dirname "$0")/deploy.sh"' >> /root/.bashrc
-        source /root/.bashrc
-    fi
-else
-    echo "-> Please execute as root !"
+    echo 'alias deploy="bash ~/site-deployer/deploy.sh"' >> /root/.zshrc
+    source /root/.zshrc
+elif [ -f ~/.bashrc ]
+then
+    echo 'alias deploy="bash ~/site-deployer/deploy.sh"' >> /root/.bashrc
+    source /root/.bashrc
 fi
