e77ccaf58914f07ff94ddf50b70d85a445b26a6f
Martin Bouillaud <martin.bouillaud@fr.clara.net>
2019-05-10 17:40:23 +0200

    Fix paths

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # shellcheck disable=SC1091
-. /etc/environment
-source ${SCRIPT_PATH}/functions/vars.sh
-source ${SCRIPT_PATH}/functions/system.sh
-source ${SCRIPT_PATH}/functions/common.sh
+source /etc/environment
+source $(dirname "$0")/functions/vars.sh
+source $(dirname "$0")/functions/system.sh
+source $(dirname "$0")/functions/common.sh
 IPV4=$(curl -s ip4.clara.net)
 export IPV4
 
